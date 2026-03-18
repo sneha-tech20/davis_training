adc7a6fa30dc2e13687aefd0e25af8395be82e8c
sneha-tech20 <sneha.y722765@gmail.com>
2026-03-18 11:19:48 +0530

    Add files via upload

---
+++ Data Visualization/coding_challenge/evennumber6.py
@@ -0,0 +1,4 @@
+n=int(input("Enter the last value till u want even nummber : "))
+for num in range(1,n):  #iteration to find even numbers
+    if num%2==0:    #logic to check even or not
+        print(num)  #if even print numbers
