6022ca52156ee2efc1f1e1a5e97794f57c95a364
sneha-tech20 <sneha.y722765@gmail.com>
2026-03-18 10:23:24 +0530

    Add files via upload

---
+++ Data Visualization/coding_challenge/diwscountbill2.py
@@ -0,0 +1,9 @@
+current_bill=int(input("Enter the amount of your bill : "))  #bill before discount
+discount=int(input("Enter discount in percentage : "))  #discount in %
+
+# logic 
+
+disc_amount=(discount/100)*current_bill  
+
+paid_bill=current_bill-disc_amount
+print(paid_bill)  #amount to be paid
