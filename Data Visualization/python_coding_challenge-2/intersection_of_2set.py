404a35173dd8b9b390e25b5df8507c58321d025a
Sneha yadav <sneha.y722765@gmail.com>
2026-04-09 10:44:41 +0530

    Add files via upload

---
+++ Data Visualization/python_coding_challenge-2/intersection_of_2set.py
@@ -0,0 +1,9 @@
+# Program to find intersection of two sets
+
+set1 = {1, 2}
+set2 = {2, 3}
+
+# intersection using & operator
+result = set1 & set2
+
+print(result)
---
+++ Data Visualization/python_coding_challenge-2/list_to_tuple.py
@@ -0,0 +1,8 @@
+# Program to convert list to tuple
+
+nums = [1, 2, 3]
+
+# convert using tuple()
+t = tuple(nums)
+
+print(t)
